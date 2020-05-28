4eb422f8163ff6c14795e0fb47b328f1cf94b157
Marcelle Samir <53565795+MarcelleSamir@users.noreply.github.com>
2020-05-28 02:58:40 +0200

    adding new features
    select between predefined multiple serires

--- next_episode.sh
+++ next_episode.sh
@@ -1,45 +1,70 @@
 #!/bin/bash
 
-ls -lut --time=atime /home/$USER/FRIENDS > watching_history.txt
+read -p "please enter one of (friends, himym): " series_name
+ls -lut --time=atime /media/$USER/EE422C35422C0545/Movies/$series_name > /home/$USER/next_episode_repo/next_episode/watching_history.txt
 
-sed -n '2{p;q}' watching_history.txt > last_episode_line.txt
+sed -n '2{p;q}' /home/$USER/next_episode_repo/next_episode/watching_history.txt > /home/$USER/next_episode_repo/next_episode/last_episode_line.txt
 
-res0=`grep -oP "friends.s\d*e\d*" last_episode_line.txt` 
-echo "$res0" > last_episode_line.txt
+res0=`grep -oP "$series_name.s\d*e\d*" /home/$USER/next_episode_repo/next_episode/last_episode_line.txt` 
+echo "$res0" > /home/$USER/next_episode_repo/next_episode/last_episode_line.txt
 
-res1=`grep -oP "\d*e\d*" last_episode_line.txt`
-echo "$res1" > last_episode_line.txt
+res1=`grep -oP "\d*e\d*" /home/$USER/next_episode_repo/next_episode/last_episode_line.txt`
+echo "$res1" > /home/$USER/next_episode_repo/next_episode/last_episode_line.txt
 
-res2=`grep -oP "\d*" last_episode_line.txt`
-echo "$res2" > last_episode_line.txt
+res2=`grep -oP "\d*" /home/$USER/next_episode_repo/next_episode/last_episode_line.txt`
+echo "$res2" > /home/$USER/next_episode_repo/next_episode/last_episode_line.txt
 
-season_num=`sed -n '1{p;q}' last_episode_line.txt`
+season_num=`sed -n '1{p;q}' /home/$USER/next_episode_repo/next_episode/last_episode_line.txt`
 
-episode_num=`sed -n '2{p;q}' last_episode_line.txt`
+episode_num=`sed -n '2{p;q}' /home/$USER/next_episode_repo/next_episode/last_episode_line.txt`
 
-	if [[ ${season_num} == "3" ]]
+if [[ $series_name -eq "friends" ]]
+then
+	if [[ ${season_num} -eq "3" ]]
 	then
 		max_episode=25
-	elif [[ ${season_num} == "6" ]]
+	elif [[ ${season_num} -eq "6" ]]
 	then
 		max_episode=25
-	elif [[ ${season_num} == "9" ]]
+	elif [[ ${season_num} -eq "9" ]]
 	then
 		max_episode=23
- 	elif  [[ ${season_num} == "10" ]]
+ 	elif  [[ ${season_num} -eq "10" ]]
 	then	
 		max_episode=17
 	else
 		max_episode=24
 	fi
-			
+	max_seasons=10
+
+elif [[ $series_name -eq "himym" ]]
+then
+	if [[ ${season_num} == "1" ]]
+	then
+		max_episode=22
+	elif [[ ${season_num} == "2" ]]
+	then
+		max_episode=22
+	elif [[ ${season_num} == "3" ]]
+	then
+		max_episode=20
+	else
+		max_episode=24
+	fi
+	max_seasons=9
+else
+
+echo "wrong choise, try again"
+fi
+
+		
 if [[ "$episode_num" -lt "$max_episode" ]]
 then
 	next_episode=$(($episode_num+1))
 	next_season="$season_num"
 
 else
-	if [[ $season_num -lt "10" ]]
+	if [[ "$season_num" -lt "$max_seasons" ]]
   	then
 		next_season=$(($season_num+1))
 		next_episode="1"
@@ -49,5 +74,13 @@ else
 	fi
 fi
 
-final_num=friends.s"$next_season"e"$next_episode".mkv
-vlc "/home/marcelle/FRIENDS/$final_num"
+# for debugging
+#echo $max_episode > /home/$USER/next_episode_repo/next_episode/hello.txt
+#echo $season_num >> /home/$USER/next_episode_repo/next_episode/hello.txt
+#echo $episode_num >> /home/$USER/next_episode_repo/next_episode/hello.txt
+#echo $next_season >> /home/$USER/next_episode_repo/next_episode/hello.txt
+#echo $next_episode >> /home/$USER/next_episode_repo/next_episode/hello.txt
+#echo $max_seasons >> /home/$USER/next_episode_repo/next_episode/hello.txt
+
+final_num="$series_name".s"$next_season"e"$next_episode".mkv
+vlc "/media/$USER/EE422C35422C0545/Movies/$series_name/$final_num"
