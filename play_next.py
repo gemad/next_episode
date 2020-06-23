4977ea40afceaef98d319bf021fa7fe4d6d7bc76
Marcelle Samir <53565795+MarcelleSamir@users.noreply.github.com>
2020-06-23 20:05:48 +0200

    Update play_next.py

--- play_next.py
+++ play_next.py
@@ -1,12 +1,34 @@
 import os
+import stat
+import time
+
+def main():
+    f=open("F:\Movies\history.txt", 'w')
+    f.write("")
+    f.close()
+    f=open("F:\Movies\history.txt", 'a+')
+    for i in os.listdir("F:\Movies\himym"):
+        #a_string = "F:\Movies\himym\"
+        x = "F:\Movies\himym\\"
+        f.write(i)
+        
+        fileStatsObj = os.stat ( x+i )
+        accessTime = time.ctime ( fileStatsObj [ stat.ST_ATIME ] )
+        f.write("  "+accessTime+"\n")
+    f.close()
+
+if __name__ == '__main__':
+    main()
+
+#import os
 #to know the current operating system ,
 # use it to change the play command according to the OS 
-import platform
-print (platform.system())
+#import platform
+#print (platform.system())
 
-Linux_player_command="vlc" 
-current_season=""
-current_episode=""
+#Linux_player_command="vlc" 
+#current_season=""
+#current_episode=""
 
 #1)read the file to update the current season and epsode 
 #2)generate the reset of the palyer command 
